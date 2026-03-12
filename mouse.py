ee08f6a18c15f5768a43eb13e0278b28b0b09d0a
Michael Bunte <michaelwbunte@gmail.com>
2026-03-11 19:58:17 -0700

    a 'cat' that can't move

--- mouse.py
+++ mouse.py
@@ -8,12 +8,74 @@ UPDATE_MS = 10
 CAMERA_SPEED = 5
 
 class Point:
-    def __init__(self, x:float, y:float):
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
 
+    @classmethod
+    def from_angle(cls, rot: float, offset: float):
+        return cls(
+            math.cos(rot) * offset,
+            math.sin(rot) * offset
+        )
+
     def copy(self):
         return Point(self.x, self.y)
+
+    def _coerce(self, other):
+        if isinstance(other, Point):
+            return other.x, other.y
+        if isinstance(other, (int, float)):
+            return other, other
+        return NotImplemented
+
+    def __add__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(self.x + ox, self.y + oy)
+
+    def __sub__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(self.x - ox, self.y - oy)
+
+    def __mul__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(self.x * ox, self.y * oy)
+
+    def __truediv__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(self.x / ox, self.y / oy)
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __rsub__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(ox - self.x, oy - self.y)
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __rtruediv__(self, other):
+        vals = self._coerce(other)
+        if vals is NotImplemented:
+            return NotImplemented
+        ox, oy = vals
+        return Point(ox / self.x, oy / self.y)
 
 
 def get_direction(p1: Point, p2: Point):
@@ -219,8 +281,8 @@ class ObstacleContainer:
 
         min_x = camera.x
         min_y = camera.y
-        max_x = camera.x + screen_size.y
-        max_y = camera.y + screen_size.x
+        max_x = camera.x + screen_size.x
+        max_y = camera.y + screen_size.y
 
         ids = self.__index.intersection((min_x, min_y, max_x, max_y))
         return [self.__obstacles[i][0] for i in ids]
@@ -233,18 +295,83 @@ class ObstacleContainer:
 
         for obstacle in obstacles:
             obstacle.draw(screen, camera_position)
+    
+class Cat:
+    def __init__(self, global_pos=Point(0, 0)):
+        self.__head_rot = 0.0
+        self.__body_rot = 0.0
+        self.global_pos = global_pos.copy()
+        self.max_rot_delta = 0.08  # radians per tick
 
-    def get_obstacles_on_screen(self, game):
-        camera = game.get_camera_position()
+    def get_position(self):
+        return self.global_pos.copy()
+
+    def draw(self, game):
+        head = Polygon(
+            global_position=(self.global_pos + Point.from_angle(self.__body_rot, 30)),
+            rot=self.__head_rot,
+            size=20,
+            points=[
+                Point(0, -1),
+                Point(0, 1),
+                Point(2, 0)
+            ],
+            color=(247, 134, 27)
+        )
+
+        body = Polygon(
+            global_position=self.global_pos,
+            rot=self.__body_rot,
+            size=20,
+            points=[
+                Point(-2, -0.6),
+                Point(-2, 0.6),
+                Point( 2, 0.6),
+                Point( 2, -0.6)
+            ],
+            color=(255, 146, 56)
+        )
+
+        body.draw(
+            game.get_screen(),
+            game.get_camera_position()
+        )
+
+        head.draw(
+            game.get_screen(),
+            game.get_camera_position()
+        )
+
+    def _wrap_angle(self, angle):
+        while angle > math.pi:
+            angle -= 2.0 * math.pi
+        while angle < -math.pi:
+            angle += 2.0 * math.pi
+        return angle
+
+    def tick(self, game):
         screen_size = game.get_screen_size()
+        screen_center = screen_size / 2
 
-        min_x = camera.x
-        min_y = camera.y
-        max_x = camera.x + screen_size.y
-        max_y = camera.y + screen_size.x
+        mx, my = pygame.mouse.get_pos()
+        mouse_pos = Point(mx, my)
 
-        ids = self.__index.intersection((min_x, min_y, max_x, max_y))
-        return [self.__obstacles[i][0] for i in ids]
+        to_mouse = mouse_pos - screen_center
+
+        # Avoid unstable atan2(0, 0) behavior when cursor is exactly centered
+        if to_mouse.x == 0 and to_mouse.y == 0:
+            return
+
+        target_rot = math.atan2(to_mouse.y, to_mouse.x)
+
+        angle_diff = self._wrap_angle(target_rot - self.__head_rot)
+
+        if angle_diff > self.max_rot_delta:
+            angle_diff = self.max_rot_delta
+        elif angle_diff < -self.max_rot_delta:
+            angle_diff = -self.max_rot_delta
+
+        self.__head_rot = self._wrap_angle(self.__head_rot + angle_diff)
 
 class Game:
     def __init__(self):
@@ -252,18 +379,23 @@ class Game:
         self.__obstacle_container = ObstacleContainer()
         self.__clock = pygame.time.Clock()
         self.__last_update = pygame.time.get_ticks()
-        self.__current_screen_size = Point(600, 800)
+        self.__cat = Cat()
         
         pygame.init()
-        self.__screen = pygame.display.set_mode((self.__current_screen_size.y, self.__current_screen_size.x))
+        self.__current_screen_size = Point(800, 600)
+        self.__screen = pygame.display.set_mode((
+            int(self.__current_screen_size.x),
+            int(self.__current_screen_size.y)
+        ))
 
         generate_random_map(
-            seed=12345,
+            seed=0,
             width=10000,
             height=10000,
             object_count=4000,
             obstacle_container=self.__obstacle_container
         )
+
         self.__run_game()
     
     def get_screen(self):
@@ -283,6 +415,7 @@ class Game:
 
             self.__screen.fill((30, 30, 30))
             self.__obstacle_container.draw_obstacles(self)
+            self.__cat.draw(self)
             pygame.display.flip()
             self.__clock.tick(120)
 
@@ -293,18 +426,13 @@ class Game:
         if mouse_buttons[0]:
             mx, my = pygame.mouse.get_pos()
 
-        if keys[pygame.K_LEFT]:
-            self.__camera_position.x -= CAMERA_SPEED
-        if keys[pygame.K_RIGHT]:
-            self.__camera_position.x += CAMERA_SPEED
-        if keys[pygame.K_UP]:
-            self.__camera_position.y -= CAMERA_SPEED
-        if keys[pygame.K_DOWN]:
-            self.__camera_position.y += CAMERA_SPEED
+        self.__cat.tick(self)
 
 
     def get_camera_position(self):
-        return self.__camera_position.copy()
+        # return self.__camera_position.copy()
+            return self.__cat.get_position() - (self.__current_screen_size / 2)
+        # return self.__cat.get_position()
     
     def get_screen_size(self):
         return self.__current_screen_size.copy()
